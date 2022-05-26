fa17eb7e9c17404fd223e36e9988481a4bf4f324
hiroyuki_yamashita <hirokore3221@gmail.com>
2022-05-26 22:44:15 +0900

    first commit

---
+++ src/test/java/com/example/demo/SpringSecrity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecrity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
